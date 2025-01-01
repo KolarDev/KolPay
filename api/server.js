173f3dca2ac87d7ea5ad1ae310d9df8d235ff2ce
KolarDev <muhyideenabdulbasit11@gmail.com>
2025-01-01 02:46:39 +0000

    production

--- api/server.js
+++ api/server.js
@@ -2,9 +2,9 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
 dotenv.config({ path: './config.env' });
-const app = require('./src/app');
+const app = require('../src/app');
 
-require('./db/database');
+require('../db/database');
 
 const port = process.env.PORT || 7000;
 
